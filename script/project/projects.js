14a39f457cabe0894b5f3629fd702a5524c0de79
Jithin Sunildas <jithinsunildas6@gmail.com>
2025-08-28 20:31:07 +0530

    update: fixed navbar redirection in gallery, events css, updateed ongoing banner, added projects

--- script/project/projects.js
+++ script/project/projects.js
@@ -4,11 +4,37 @@ const projects = [
   {
     title: "Space Station Simulator",
     description: "Virtual reality simulation of International Space Station operations and zero-gravity experiments.",
-    image: "data:image/svg+xml;base64,...",
+    image: "assets/projects/project-1.png",
     tags: ["VR/AR", "Simulation", "Education"],
     isOngoing: true,
     liveLink: "#",
     githubLink: "#"
   },
-  
+  {
+    title: "Cansat",
+    description: "A CanSat is a miniaturized satellite that’s built to fit inside the volume of a soda can. It giveshands-on experience with designing, building, and testing a satellite system at a much lower cost and complexity than a real satellite.",
+    image: "assets/projects/project-2.webp",
+    tags: ["Electronics ", "RF communication", "microcontroller", "C programming", "3d Designing/printing", "parachute designing"],
+    isOngoing: true,
+    liveLink: "#",
+    githubLink: "#"
+  },
+  {
+    title: "CTF Project",
+    description: "It's a type of cybersecurity competition where participants solve challenges to find hidden pieces of data called flags(usually in the format like flag{something_secret}).",
+    image: "assets/projects/project-3.webp",
+    tags: ["CLI", "Networking protocols", "network scanning"],
+    isOngoing: true,
+    liveLink: "#",
+    githubLink: "#"
+  },
+  {
+    title: "PROJECT ATL",
+    description: "it is a humanitarian projects we take classes for school students abount electronics, microcontrollers etc.. we guide them to develop their technical skills for the IT MELA.",
+    image: "assets/projects/project-4.png",
+    tags: ["Teaching", "leadership", "technical knowledge", "connections"],
+    isOngoing: true,
+    liveLink: "#",
+    githubLink: "#"
+  },
 ];
