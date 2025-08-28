10f14eada49ba4db031c11c0f82926afcf0f897d
Ashwin <achuashwin049@gmail.com>
2025-08-28 20:50:35 +0530

    fix : execom, footer

--- script/events/events.js
+++ script/events/events.js
@@ -1,14 +1,29 @@
 const eventsItems = [
   {
-    title: "Boot-Camp Phase 1",
-    desc: "A basic electronic workshop.",
+    title: "8 Day Boot-Camp (Phase 1)",
+    desc: "An 8-day hands-on journey into the world of communication engineering. <br> Covered: Basics of electronics, resistors, capacitors & communication systems (Days 1–2). <br> Next: Diving deeper each day, leading to the design and build of an antenna by the end of Phase One. <br> Learn by doing. <br><br> Build from basics to real engineering.",
     image: "assets/gallery/2.jpeg",
   },
   {
-    title: "Outreach programme",
-    desc: "Guidance for school students for IT fair",
-    image: "assets/gallery/3.jpg",
-  },
+  title: "Circuit Designing Competition",
+  desc: "As part of Phase One, we conducted a hands-on circuit designing competition where participants applied their basics of electronics to create innovative circuits and showcase their problem-solving skills.",
+  image: "assets/events/ckt.jpeg",
+},
+{
+  title: "Coding Competition",
+  desc: "We organized a Coding Competition where students showcased their skills in C, C++ and Java through problem-solving challenges. The event encouraged logical thinking, speed, and creativity. After intense rounds, four winners emerged and celebrated with an Alfahm Mandi treat, making the competition both exciting and memorable.",
+  image: "assets/events/coding.jpeg",
+},
+
+
+ {
+  title: "Outreach Programme",
+  desc: "Session at GHSS Valad introducing students to basic electronics, communication, and microcontrollers — sparking interest and giving them a strong first step into the world of technology.",
+  image: "assets/gallery/3.jpg",
+}
+
+
+
 ];
 
 const container = document.querySelector(".events-container");
--- script/execom/execom.js
+++ script/execom/execom.js
@@ -10,12 +10,37 @@ const execomMembers = [
     instagram: "https://www.instagram.com/__.sonaahhh.__?igsh=MXR3bWp1MmJ0eGgyMA=="
   },
   {
-    name: "Ashwin",
+    name: "Ashwin Dev",
     title: "Vice-President",
     image: "assets/execom/ashwin.jpg",
     linkedin: "https://www.linkedin.com/in/ashwin-8a8070241?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     github: "https://github.com/Ashwin04912",
     instagram: "https://www.instagram.com/soul_worker__049?igsh=aHEycTBuOGplOHNr"
+  },
+
+   {
+    name: "Sreelakshmi",
+    title: "Secretary",
+    image: "assets/execom/sreelakshmi.jpeg",
+    linkedin: "#",
+    github: "#",
+    instagram: "https://www.instagram.com/sreel.a.k.shmi?igsh=MW1pcXY2NnJ6Mnc1aQ=="
+  },
+  {
+    name: "Dhanesh SP",
+    title: "Project Head",
+    image: "assets/execom/dhanesh.jpg",
+    linkedin: "https://www.linkedin.com/in/dhanesh-sp-452986257",
+    github: "https://github.com/dhanesh221",
+    instagram: "#"
+  },
+  {
+    name: "Sreya C",
+    title: "Event Coordinator",
+    image: "assets/execom/sreya.jpeg",
+    linkedin: "https://www.instagram.com/sreya_c_r?igsh=eW1wMWRya29meDZn",
+    github: "https://github.com/sreya-c-r",
+    instagram: "https://www.linkedin.com/in/sreya-c-1a13ab270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
   },
   {
     name: "Jithin Sunildas",
@@ -27,14 +52,43 @@ const execomMembers = [
   },
   {
     name: "Shriya K",
-    title: "Membership Coordinator ",
+    title: "Membership Coordinator",
     image: "assets/execom/shriyaganesh.jpg",
     linkedin: "https://www.linkedin.com/in/shriya-k-b8261236b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
     github: "#",
     instagram: "https://www.instagram.com/shri_shriya/profilecard/?igsh=MmNvZWp6dm5jNDcx"
   },
   // Add more members here
+ 
   {
+    name: "Sahla K P",
+    title: "Public Relations Officer",
+    image: "assets/execom/sahlarahman.jpg",
+    linkedin: "https://www.linkedin.com/in/sahla-rahman-bb52782a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    github: "#",
+    instagram: "https://www.instagram.com/sahla.rahman_?igsh=MXhkMjI3djg3enJicw=="
+  },
+
+
+  {
+    name: "Albin Shaji",
+    title: "Treasurer",
+    image: "assets/execom/albin_shaji.jpeg",
+    linkedin: "https://www.linkedin.com/in/albin-shaji-784bb5283?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    github: "#",
+    instagram: "#"
+  },
+
+  {
+    name: "Pranav SR",
+    title: "Event Committee Member",
+    image: "assets/execom/pranavsr.jpg",
+    linkedin: "https://www.linkedin.com/in/pranav-s-r-7267b0296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    github: "#",
+    instagram: "https://www.instagram.com/pra_nav_s_r?igsh=MWg1d3dudzVlcjhjdg=="
+  },
+
+   {
     name: "Albin B Thomas ",
     title: "Student mentor",
     image: "assets/execom/albinbthomas.jpg",
@@ -42,22 +96,7 @@ const execomMembers = [
     github: "#",
     instagram: "https://www.instagram.com/al.b_t?igsh=MWdzdnA2bjl1dWtqdA=="
   },
-  {
-    name: "Sahla K P",
-    title: "P.R.O",
-    image: "assets/execom/sahlarahman.jpg",
-    linkedin: "https://www.linkedin.com/in/sahla-rahman-bb52782a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    github: "#",
-    instagram: "https://www.instagram.com/sahla.rahman_?igsh=MXhkMjI3djg3enJicw=="
-  },
 
-  {
-    name: "Pranav SR",
-    title: "Event Committee",
-    image: "assets/execom/pranavsr.jpg",
-    linkedin: "https://www.linkedin.com/in/pranav-s-r-7267b0296?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-    github: "#",
-    instagram: "https://www.instagram.com/pra_nav_s_r?igsh=MWg1d3dudzVlcjhjdg=="
-  },
+  
   
 ];
